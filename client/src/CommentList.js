48cea6fb6fa9a05bc8f87c488ae4e6d9653d7572
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-04-05 16:46:55 -0400

    Minimize the number of API requests

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,18 +1,4 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
-  const fetchComments = async() => {
-    const results = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-    setComments(results.data);
-  };
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
   })
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -11,7 +11,7 @@ const PostList = () => {
   }, []);
 
   const fetchPosts = async () => {
-    const results = await axios.get('http://localhost:4000/posts');
+    const results = await axios.get('http://localhost:4002/posts');
     setPosts(results.data); 
   }
 
@@ -20,7 +20,7 @@ const PostList = () => {
       <div className='card' style={{ width: '30%', marginBottom: '20px'}} key={post.id}>
         <div className='card-body'>
           <h3>{post.title}</h3>
-          <CommentList postId={post.id}/>
+          <CommentList comments={post.comments}/>
           <CommentCreate postId={post.id}/>
           
         </div>
